9f547be6939330535f6c46ed59a76a65935ce23d
Liminal Pepe <theliminalguy@gmail.com>
2023-05-23 23:19:39 -0300

    prepare navigation menus and routing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,44 +1,42 @@
-import { ethers } from 'ethers';
 import logo from '../assets/logo.svg';
+import { Link } from 'react-router-dom';
 
 const Navigation = ({ account, setAccount }) => {
-    const connectHandler = async () => {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        const account = ethers.utils.getAddress(accounts[0])
-        setAccount(account);
-    }
+  const connectHandler = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setAccount(accounts[0]);
+  }
+  return (
+    <nav>
+      <ul className='nav__links'>
+        <li><Link to="/">Buy</Link></li>
+        <li><Link to="/sell">Sell</Link></li>
+      </ul>
 
-    return (
-        <nav>
-            <ul className='nav__links'>
-                <li><a href="#">Buy</a></li>
-                <li><a href="#">Rent</a></li>
-                <li><a href="#">Sell</a></li>
-            </ul>
+      <div className='nav__brand'>
+        <img src={logo} alt="Logo" />
+        <h1>Millow</h1>
+      </div>
 
-            <div className='nav__brand'>
-                <img src={logo} alt="Logo" />
-                <h1>Millow</h1>
-            </div>
+      {account ? (
+        <button
+        type="button"
+        className='nav__connect'
+        >
+          {account.slice(0, 6) + '...' + account.slice(38, 42)}
+        </button>
+      ): (
+        <button
+        type="button"
+        className='nav__connect'
+        onClick={connectHandler}
+        >
+          Connect
+        </button>
+      )}
 
-            {account ? (
-                <button
-                    type="button"
-                    className='nav__connect'
-                >
-                    {account.slice(0, 6) + '...' + account.slice(38, 42)}
-                </button>
-            ) : (
-                <button
-                    type="button"
-                    className='nav__connect'
-                    onClick={connectHandler}
-                >
-                    Connect
-                </button>
-            )}
-        </nav>
-    );
+    </nav>
+  );
 }
 
 export default Navigation;
